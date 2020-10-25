9d4f06b32a204992abd37d168e8ec350f1de9ff0
Paul <webdevpaulmason@gmail.com>
2020-10-25 16:04:05 -0700

    Setup for Heroku

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const passport = require("passport");
 const users = require("./routes/api/users");
-const friends = require("./routes/crud/friends/friends")
-const User = require('./models/User');
+const friends = require("./routes/crud/friends/friends");
+const User = require("./models/User");
+
+require("dotenv").config();
 
 //Config
 app.use(express.json());
@@ -16,33 +18,27 @@ app.use(cors());
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+// const db = require("./config/keys").mongoURI;
+const db = process.env.MONGOURI;
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-  // Passport middleware
+// Passport middleware
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
-app.use("/friends", friends)
+app.use("/friends", friends);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
-
-
